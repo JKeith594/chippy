89d3148715e81aba3aef6bdcf298d87546796844
James <jimmy4222@gmail.com>
2021-02-08 21:52:30 -0500

    Initial Commit

---
+++ Shared/chippyApp.swift
@@ -0,0 +1,17 @@
+//
+//  chippyApp.swift
+//  Shared
+//
+//  Created by James Keith on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct chippyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
